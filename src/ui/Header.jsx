98c5f224894be0606f76d949b2d8c3543695c5c0
YoussefSalama4 <ahmdsalama4@gmail.com>
2024-10-15 14:37:48 +0300

    edit importing component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import SearchOrder from '../features/order/searchOrder';
+import SearchOrder from '../features/order/SearchOrder';
 import Username from '../features/user/Username';
 
 function Header() {
